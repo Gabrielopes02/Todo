c5ae78df5543bd3715720d93ab5af88461ade07d
Gabrielopes02 <tecnicogabrielopes@gmail.com>
2025-04-04 19:18:11 -0300

    colocando funcionalidade de riscar o texto

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
 
-
+var i = 0
 function adicionartask(){
     let lista = document.querySelector('ul')
     let text = document.getElementById('texto')
-    
     let novatask = document.createElement('li')
-    novatask.innerHTML = `<input type="checkbox" class="check" onclick = "excluirtask(this)"> ${text.value}`
+    novatask.setAttribute("id",i)
+    novatask.innerHTML = `<input type="checkbox" class="check"  onclick = "concluirtask(${novatask.id})"> ${text.value}`
     lista.appendChild(novatask)
+   i++
    text.value = ''
 }
 
+var estadotexto = false
+function concluirtask(id){
+  
+  let input = document.getElementById(id)
+  let texto = input.textContent
+  
+if(estadotexto == false){
 
-function excluirtask(li){
-li.parentElement.remove()
+  input.innerHTML = `<input type="checkbox" class="check"  onclick = "concluirtask(${id})"><s>${texto}</s> `
+  estadotexto = true
+  
+}
+else{
+    
+    input.innerHTML = `<input type="checkbox" class="check"  onclick = "concluirtask(${id})">  ${texto}`
+    estadotexto = false
+}
+
+
+
+
+
+
 }
